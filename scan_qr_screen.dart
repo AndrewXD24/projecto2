90a127bb51713825b310f666849420733b48e7f0
NormanG <normanguar7@gmail.com>
2026-03-14 19:59:24 -0600

    Create scan_qr_screen.dart

---
+++ scan_qr_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+class ScanQRScreen extends StatelessWidget {
+  const ScanQRScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Escanear QR"),
+      ),
+
+      body: MobileScanner(
+
+        onDetect: (BarcodeCapture capture) {
+
+          final String? code = capture.barcodes.first.rawValue;
+
+          if (code != null) {
+            Navigator.pop(context, code);
+          }
+
+        },
+
+      ),
+    );
+  }
+}
