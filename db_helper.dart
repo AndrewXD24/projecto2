d993391fa2538613987ee285550753def46f0c9b
NormanG <normanguar7@gmail.com>
2026-03-20 14:21:54 -0600

    Implement user login and password hashing
    Added user authentication functionality with password hashing and a default admin user.

--- db_helper.dart
+++ db_helper.dart
@@ -1,20 +1,18 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import 'dart:convert';
+import 'package:crypto/crypto.dart';
 
 class DBHelper {
-
   static Database? _database;
 
   Future<Database> get database async {
-
     if (_database != null) return _database!;
-
     _database = await initDB();
     return _database!;
   }
 
   Future<Database> initDB() async {
-
     String path = join(await getDatabasesPath(), 'inventario.db');
 
     return await openDatabase(
@@ -22,6 +20,7 @@ class DBHelper {
       version: 1,
       onCreate: (db, version) async {
 
+        // PRODUCTOS
         await db.execute('''
         CREATE TABLE productos(
           id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -31,8 +30,38 @@ class DBHelper {
         )
         ''');
 
+        // USUARIOS
+        await db.execute('''
+        CREATE TABLE usuarios(
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          usuario TEXT UNIQUE,
+          password TEXT
+        )
+        ''');
+
+        // ADMIN
+        await db.insert("usuarios", {
+          "usuario": "admin",
+          "password": sha256.convert(utf8.encode("1234")).toString()
+        });
       },
     );
+  }
+
+  String hashPassword(String password) {
+    return sha256.convert(utf8.encode(password)).toString();
+  }
+
+  Future<bool> loginUsuario(String user, String pass) async {
+    final db = await database;
+
+    final result = await db.query(
+      "usuarios",
+      where: "usuario = ? AND password = ?",
+      whereArgs: [user, hashPassword(pass)],
+    );
+
+    return result.isNotEmpty;
   }
 
   Future<void> insertarProducto(Map<String, dynamic> producto) async {
