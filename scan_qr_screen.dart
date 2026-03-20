86ba2fbcbb8dc1b75ea4d33715ad989d51cfc435
NormanG <normanguar7@gmail.com>
2026-03-20 14:17:11 -0600

    Update scan_qr_screen.dart

--- scan_qr_screen.dart
+++ scan_qr_screen.dart
@@ -6,24 +6,16 @@ class ScanQRScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Escanear QR"),
-      ),
-
+      appBar: AppBar(title: const Text("Escanear QR")),
       body: MobileScanner(
-
         onDetect: (BarcodeCapture capture) {
-
           final String? code = capture.barcodes.first.rawValue;
 
           if (code != null) {
             Navigator.pop(context, code);
           }
-
         },
-
       ),
     );
   }
